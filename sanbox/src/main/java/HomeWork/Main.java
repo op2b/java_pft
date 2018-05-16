542d39a22a69cdaeee1e6917141abf4a3bc61a6e
op2b <rsupanda@gmail.com>
2018-05-16 20:38:38 +0300

    конец ракокодинга.

---
+++ sanbox/src/main/java/HomeWork/Main.java
@@ -0,0 +1,10 @@
+package HomeWork;
+
+class Main{
+  public static void main(String[] args){
+    Point p1 = new Point(12,13);
+    Point p2 = new Point(76,57);
+    System.out.println("Расстояние = " + Point.distance(p1,p2));
+  }
+}
+
--- sanbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sanbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,23 +4,18 @@ public class MyFirstProgram {
 
  public static void main (String[] args) {
   hello("world");
-double len = 5;
-  System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+Square s = new Square(5);
+s.l=5;
 
-  double a = 4;
-  double b = 6;
-  System.out.println("Площадь прямоугольника со сторонами " + a + "и" + b + " = " +  area(a, b));
+  System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+Rectangle r= new Rectangle(4,6);
+
+  System.out.println("Площадь прямоугольника со сторонами " + r.a + "и" + r.b + " = " +  r.area());
  }
  public static void hello(String somebody ) {
   System.out.println("hello " + somebody + "!");
 
   }
-  public static double  area (double l) {
- return l * l;
- }
 
- public static double area (double a, double b) {
-  return a*b ;
- }
 
 }
